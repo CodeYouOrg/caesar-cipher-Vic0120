2214bb86bdfb2a0cc55acb9d39db0a6460b11004
rose <rose_g3@yahoo.com>
2024-08-03 16:21:54 -0400

    Updated code

--- cipher.py
+++ cipher.py
@@ -1,1 +1,35 @@
-# add your code here
+ceasar = {
+    "A" : "E",
+    "B" : "F",
+    "C" : "G",
+    "D" : "H",
+    "E" : "I",
+    "F" : "J",
+    "G" : "K",
+    "H" : "L",
+    "I" : "M",
+    "J" : "N",
+    "K" : "O",
+    "L" : "P",
+    "M" : "Q",
+    "N" : "R",
+    "O" : "S",
+    "P" : "T",
+    "Q" : "U",
+    "R" : "V",
+    "S" : "W",
+    "T" : "X",
+    "U" : "Y",
+    "V" : "Z",
+    "W" : "A",
+    "X" : "B",
+    "Y" : "C",
+    "Z" : "D",
+    " " : " "
+}
+
+sentence = input("Please enter a sentence to disguise: " )
+sentence = sentence.upper()
+for letter in sentence:
+    letter = ceasar.get(letter)
+    print (letter)
